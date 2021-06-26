46cc980081ff99a6ae2a5a76f9b6ef04531b61fb
tkuriyama <taro.kuriyama@gmail.com>
2021-06-26 14:18:56 -0400

    initial interface complete

--- blockchain/toycoin/merkle.py
+++ blockchain/toycoin/merkle.py
@@ -1,10 +1,15 @@
 # Merkle Hash Tree
 
 from toycoin import hash
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 
 ################################################################################
+
+
+Hash = bytes
+
+
 
 class MerkleTree:
     """Merkle hash tree.
@@ -12,7 +17,7 @@ class MerkleTree:
     """
 
     def __init__(self,
-                 label: bytes,
+                 label: Hash,
                  left: Optional['MerkleTree'] = None,
                  right: Optional['MerkleTree'] = None
                  ):
@@ -26,35 +31,35 @@ class MerkleTree:
                   left: Optional['MerkleTree'] = None,
                   right: Optional['MerkleTree'] = None
                   ) -> int:
-            """"""
-            if left is not None and right is not None:
-                size = left.size + right.size
-            elif left is not None:
-                size = left.size
-            else:
-                size = 1
-            return size
+        """Helper for initializing size."""
+        if left is not None and right is not None:
+            size = left.size + right.size + 1
+        elif left is not None:
+            size = left.size + 1
+        else:
+            size = 1
+        return size
 
 
-    def insert(self, label: bytes):
-        """Recurse insertion; return count of new nodes from insertion."""
+    def insert(self, leaf: Hash):
+        """Recurse insertion."""
         # base cases
         if self.left is None:
-            self.left = MerkleTree(b'\x00' + label)
+            self.left = MerkleTree(b'\x00' + leaf)
             self.update()
 
         elif self.right is None:
-            self.right = MerkleTree(b'\x00' + label)
+            self.right = MerkleTree(b'\x00' + leaf)
             self.update()
 
         elif self.left.size == self.right.size:
             self.left = MerkleTree(self.label, self.left, self.right)
-            self.right = from_singleton(label)
+            self.right = from_singleton(leaf)
             self.update()
 
         # recursive case
         else:
-            self.right.insert(label)
+            self.right.insert(leaf)
             self.update()
 
 
@@ -68,6 +73,7 @@ class MerkleTree:
         else:
             labels = self.left.label + self.right.label
             size_ = self.left.size + self.right.size
+
         self.label = b'\x01' + hash.hash(labels)
         self.size = 1 + size_
 
@@ -76,23 +82,23 @@ class MerkleTree:
 # Constructors
 
 
-def from_singleton(label: bytes) -> MerkleTree:
-    """Create singleton Merkle Tree, with root and one leaf."""
-    leaf = MerkleTree(b'\x00' + label)
-    root = MerkleTree(b'', leaf)
+def from_singleton(leaf: Hash) -> MerkleTree:
+    """Create singleton Merkle tree, with root and one leaf."""
+    leaf_node = MerkleTree(b'\x00' + leaf)
+    root = MerkleTree(b'', leaf_node)
     root.update()
     return root
 
 
-def from_list(labels: List[bytes]) -> MerkleTree:
-    """Create singleton Merkle Tree, with root and one leaf."""
-    if not labels:
-        return MerkleTree(b'')
+def from_list(leaves: List[Hash]) -> Optional[MerkleTree]:
+    """Create Merkle tree from one or more nodes."""
+    if not leaves:
+        return None
 
-    head, tail = labels[0], labels[1:]
+    head, tail = leaves[0], leaves[1:]
     t = from_singleton(head)
-    for label in tail:
-        t.insert(label)
+    for leaf in tail:
+        t.insert(leaf)
 
     return t
 
@@ -100,21 +106,87 @@ def from_list(labels: List[bytes]) -> MerkleTree:
 ################################################################################
 # Verification
 
+MaybeLeft = Optional[Hash]
+MaybeRight = Optional[Hash]
+HashTriple = Tuple[Hash, MaybeLeft, MaybeRight]
+HashPath = List[HashTriple]
 
-    
 
+def valid(tree: MerkleTree) -> bool:
+    """Return True if all tree hashes are valid."""
+    if is_leaf(tree):
+        return tree.label[0] == 0 and len(tree.label) > 1
+
+    assert tree.left is not None
+    if tree.right is None:
+        hash_ = b'\x01' + hash.hash(tree.left.label)
+        valid_ = tree.label == hash_ and valid(tree.left)
+    else:
+        hash_ = b'\x01' + hash.hash(tree.left.label + tree.right.label)
+        valid_ = tree.label == hash_ and valid(tree.left) and valid(tree.right)
+
+    return valid_
+
+
+def contains(tree: MerkleTree, leaf: Hash) -> HashPath:
+    """Find hash path to leaf (or empty path if none found)."""
+    paths = [[(tree, get_hash_triple(tree))]]
+
+    while paths:
+        path = paths.pop()
+        t, (label, _, _) = path[-1]
+        if label == b'\x00' + leaf:
+            return [triple for _, triple in path]
+        elif not is_leaf(t):
+            paths.extend(extend_path(path, t))
+
+    return []
+
+
+def extend_path(path: List[Tuple[MerkleTree, HashTriple]],
+                tree: MerkleTree,
+                ) -> List[List[Tuple[MerkleTree, HashTriple]]]:
+    """Extend path if possible."""
+    paths = []
+    for t in (tree.left, tree.right):
+        if t is not None:
+            path_ = path + [(t, get_hash_triple(t))]
+            paths.append(path_)
+    return paths
+
+
+def get_hash_triple(tree: MerkleTree) -> HashTriple:
+    """Extract Hash triple for from given tree."""
+    return (tree.label, get_label(tree.left), get_label(tree.right))
 
 
 ################################################################################
 # Helpers
 
 
-def show_tree(tree: MerkleTree, level: int = 1):
-    """Print string representation of tree."""
+def is_leaf(tree: MerkleTree) -> bool:
+    """Return True if given tree is a leaf node."""
+    return tree.left is None and tree.right is None
 
+def get_label(tree: Optional[MerkleTree]) -> Optional[Hash]:
+    """Maybe get label."""
+    maybe_label : Optional[Hash]
+    if tree is not None:
+        maybe_label = tree.label
+    else:
+        maybe_label = None
+    return maybe_label
+
+
+def show(tree: MerkleTree, level: int = 1, show_prefix: bool = False):
+    """Print string representation of tree."""
     indent = '--' * level
-    print(f'{indent} Level: {level} | Label: {tree.label.hex()}\n')
+    label = tree.label.hex() if show_prefix else tree.label[1:].hex()
+    print(f'{indent} Level {level} | Size {tree.size} | ' +
+          f'Label: {label}\n')
+
     if tree.left is not None:
-        show_tree(tree.left, level + 1)
+        show(tree.left, level + 1, show_prefix)
     if tree.right is not None:
-        show_tree(tree.right, level + 1)
+        show(tree.right, level + 1, show_prefix)
+
