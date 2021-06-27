b1bfba7649598189a0618d0044540b2157341419
tkuriyama <taro.kuriyama@gmail.com>
2021-06-26 20:30:24 -0400

    add second preimage test

--- blockchain/tests/test_merkle.py
+++ blockchain/tests/test_merkle.py
@@ -1,6 +1,6 @@
 
-from toycoin import hash
-from toycoin import merkle
+from toycoin import hash # type: ignore
+from toycoin import merkle # type: ignore
 
 
 ################################################################################
@@ -53,6 +53,22 @@ class TestMerkle:
         assert not merkle.contains(t1, hash.hash(b'10'))
 
 
-    #def test_second_preimage_attack(self):
-    #    """Test second preimage attack."""
+    def test_second_preimage_attack(self):
+        """Test second preimage attack."""
+        f = merkle.from_list
 
+        t = f([hash.hash(b'0'),
+               hash.hash(b'1'),
+               hash.hash(b'2'),
+               hash.hash(b'3'),
+               hash.hash(b'4')])
+
+        h1, h2 = t.left.label, t.right.label
+        t2 = f([h1, h2])
+
+        # the attack fails
+        assert t.label != t2.label
+
+        # just checking the attack prevention is working as expected
+        assert t.label[1:] == hash.hash(b'\x01' + h1[1:] +
+                                        b'\x01' + h2[1:])
