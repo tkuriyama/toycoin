7c954cf7375f4afb8804e44076c93fb852470480
tkuriyama <taro.kuriyama@gmail.com>
2021-07-07 15:30:41 -0400

    Refactoring Transactions

--- blockchain/toycoin/transaction.py
+++ blockchain/toycoin/transaction.py
@@ -1,65 +1,95 @@
 """Construct and verify transactions.
-Logically, a transaction is a transfer of some positive amount from A to B,
-signed by A. No mechanisms for providing change is implemented.
+
+
 """
 
 
 from cryptography.hazmat.primitives.asymmetric import rsa # type: ignore
 from toycoin import hash # type: ignore
-from toycoin import signature # type: ignore
+from toycoin import signature, utils # type: ignore
 from typing import List, Optional, TypedDict # type: ignore
 
 
 ################################################################################
 
-Hash = bytes
 
 Address = bytes
 
+
 class Transaction(TypedDict):
+    previous_hashes: List[hash.Hash]
     receiver: Address
-    amount: float
+    receiver_value: int
+    receiver_signature: signature.Signature
+    sender: Address
+    sender_change: int
+    sender_signature: signature.Signature
+
+
+class TransactionToken(TypedDict):
+    txn_hash: hash.Hash
+    holder: Address
+    value: int
     signature: signature.Signature
 
 
 ################################################################################
-# Transactions
+# Send
 
 
-def send(receiver: bytes,
-         sender: rsa.RSAPrivateKey,
-         amount: float,
-         previous_txn: Transaction
-         ) -> Transaction:
-    """Generate a new transaction."""
-    h = hash_txn(previous_txn)
-    return {'receiver': receiver,
-            'amount': amount,
-            'signature': signature.sign(sender, h + receiver)}
+def send(receiver_pub: bytes,
+         sender_pub: bytes,
+         sender_priv: rsa.RSAPrivateKey,
+         send_value: int,
+         tokens: List[TransactionToken]
+         ) -> Optional[Transaction]:
+    """Generate a send transaction.
+    Assumes token authenticity has been verified!
+    Returns None if token value is insufficient, and provides change if
+    token value is greater than the send value.
+    """
+    sum_value = sum_tokens(tokens)
 
-
-def valid(txns: List[Transaction]) -> Optional[bool]:
-    """Verify signatures of a list of transactions."""
-    if len(txns) <= 1:
+    if sum_value < send_value:
         return None
 
-    pairs = zip(txns, txns[1:])
-    return all(valid_pair(prev, this) for prev, this in pairs)
+    hs = [token['txn_hash'] for token in tokens]
+    txn : Transaction
+    txn = {'previous_hashes': hs,
+           'receiver': receiver_pub,
+           'receiver_value': send_value,
+           'receiver_signature': signature.sign(sender_priv,
+                                                b''.join(hs) + receiver_pub),
+           'sender': sender_pub,
+           'sender_change': sum_value - send_value,
+           'sender_signature': signature.sign(sender_priv,
+                                              b''.join(hs) + sender_pub)
+           }
+
+    return txn
 
 
-def valid_pair(previous: Transaction, this: Transaction) -> bool:
-    """Verify signature of one transaction."""
-    return signature.verify(this['signature'],
-                            signature.load_pub_key_bytes(previous['receiver']),
-                            hash_txn(previous) + this['receiver'])
+
+################################################################################
+# Validation
+
 
 
 ################################################################################
 # Helpers
 
 
-def hash_txn(txn: Transaction) -> Hash:
+def sum_tokens(tokens: List[TransactionToken]) -> int:
+    """Sum value of given TrasnactionTokens."""
+    return sum(token['value'] for token in tokens)
+
+
+def hash_txn(txn: Transaction) -> hash.Hash:
     """Hash Transaction."""
-    return hash.hash(txn['receiver'] +
-                     bytes(str(txn['amount']).encode('utf-8')) +
-                     txn['signature'])
+    return hash.hash(b''.join(txn['previous_hashes']) +
+                     txn['receiver'] +
+                     utils.int_to_bytes(txn['receiver_value']) +
+                     txn['receiver_signature'] +
+                     txn['sender'] +
+                     utils.int_to_bytes(txn['sender_change']) +
+                     txn['sender_signature'])
