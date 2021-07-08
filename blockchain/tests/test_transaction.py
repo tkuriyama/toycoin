9d7ac8f2bb75e7fdac21d259fe0187f0f8b4dbe7
tkuriyama <taro.kuriyama@gmail.com>
2021-07-08 15:24:26 -0400

    Refactoring txn tests

--- blockchain/tests/test_transaction.py
+++ blockchain/tests/test_transaction.py
@@ -1,40 +1,30 @@
+"""Test transaction.
+This covers helper functions; eee test_wallet.py module for the bulk of tests.
+"""
 
-from toycoin import signature, transaction # type: ignore
+
+from toycoin import transaction # type: ignore
 
 
 ################################################################################
 
 
-class TestTransaction:
+class TestTransactionHelpers:
 
-    def test_chain(self):
-        """Test chain of transactions."""
+    def test_sum_tokens(self):
+        """Test sum_tokens."""
+        f = transaction.sum_tokens
 
-        a_priv, b_priv = signature.gen_priv_key(), signature.gen_priv_key()
-        c_priv, d_priv = signature.gen_priv_key(), signature.gen_priv_key()
+        t1 = {'txn_hash': b'',
+              'owner': b'',
+              'value': 100,
+              'signature': b''}
 
-        txn0 = {'receiver': signature.get_pub_key_bytes(a_priv),
-                'amount': 100.0,
-                'signature': b'genesis_signature'
-                }
+        t2 = {'txn_hash': b'',
+              'owner': b'',
+              'value': 100,
+              'signature': b''}
 
-        txn1 = transaction.send(signature.get_pub_key_bytes(b_priv),
-                                a_priv,
-                                99.0,
-                                txn0)
-        txn2 = transaction.send(signature.get_pub_key_bytes(c_priv),
-                                b_priv,
-                                99.0,
-                                txn1)
-        txn3 = transaction.send(signature.get_pub_key_bytes(d_priv),
-                                c_priv,
-                                98.0,
-                                txn2)
-
-        # undecidable
-        assert transaction.valid([]) is None
-        assert transaction.valid([txn0]) is None
-
-        # chain
-        assert transaction.valid([txn0, txn1, txn2, txn3])
-        assert transaction.valid([txn0, txn1, txn3, txn2]) is False
+        assert f([]) == 0
+        assert f([t1]) == 100
+        assert f([t1, t2]) == 200
