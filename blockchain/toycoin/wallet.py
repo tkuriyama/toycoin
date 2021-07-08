9a6dce75716a418d23aefcde3ba0ec7818651a39
tkuriyama <taro.kuriyama@gmail.com>
2021-07-08 10:59:42 -0400

    Updating wallet for new send interface

--- blockchain/toycoin/wallet.py
+++ blockchain/toycoin/wallet.py
@@ -1,31 +1,81 @@
-"""The chain global wallet.
-The chain global wallet represents all valid coin holders and their coin
-balances.
+"""Wallet for clients on the blockchain.
+A wallet can hold coins (in the form of transactions), as well as send
+and receive coins (again, by generating or processing transactions).
 """
 
 
+from cryptography.hazmat.primitives.asymmetric import rsa # type: ignore
 from toycoin import transaction # type: ignore
-from typing import Dict # type: ignore
+from typing import List, Optional, Tuple # type: ignore
 
 
 ################################################################################
 
 
-Wallet = Dict[bytes, float]
+class Wallet:
+    """Wallet, initialized with owner's RSA keys."""
 
 
-################################################################################
+    def __init__(self,
+                 public_key: bytes,
+                 private_key: rsa.RSAPrivateKey):
+        self.wallet : List[transaction.Token] = []
+        self.pending : List[Tuple[bytes, transaction.Token]] = []
+        self.public_key = public_key
+        self.private_key = private_key
 
 
-def apply(wallet: Wallet, txns: List[transaction.Transaction]) -> Wallet
-    """Apply zero or more transactions to wallet.
-    By design, no validation is performed here.
-    """
-    for txn in txns:
-        receiver = txn['receiver']
-        if receiver in wallet:
-            wallet[receiver] += txn['amount']
-        else:
-            wallet[receiver] = txn['amount']
+    def balance(self) -> int:
+        """Return current wallet balance (exclude pending)."""
+        return sum(token['value'] for token in self.wallet)
 
-    return wallet
+
+    def send(self,
+             send_value: int,
+             receiver: bytes
+             ) -> Optional[transaction.Transaction]:
+        """Attempt to generate transaction that sends value.
+        Tokens included in the transaction are placed in pending state.
+        """
+        if send_value > self.balance():
+            return []
+
+        # FIFO
+        sum_value, i = 0, 0
+        while sum_value < send_value:
+            sum_value += self.wallet[i]['value']
+            i += 1
+
+        txn = transaction.send(receiver,
+                               self.public_key,
+                               self.private_key,
+                               send_value,
+                               self.wallet[:i])
+
+        self.pending.append((transaction.hash_txn(txn), self.wallet[:i]))
+        self.wallet = self.wallet[i:]
+
+        return txn
+
+
+    def confirm_send(self, txn_hash: bytes):
+        """Remove confirmed transaction from pending state."""
+        self.pending = [(h, tokens) for h, tokens in self.pending
+                        if h != txn_hash]
+
+
+    def reject_send(self, txn_hash: bytes):
+        """Return tokens to wallet from pending state."""
+        pending = []
+        for h, tokens in self.pending:
+            if h == txn_hash:
+                self.wallet = tokens + self.wallet
+            else:
+                pending.append((h, tokens))
+        self.pending = pending
+
+
+    def receive(self, txns: List[transaction.Transaction]):
+        """Add transactions to wallet."""
+        self.wallet += txns
+
