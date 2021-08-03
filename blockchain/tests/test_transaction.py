f9095dcea37e8ab650b6f57e560bc3a79b8dd83d
tkuriyama <taro.kuriyama@gmail.com>
2021-08-03 15:04:28 -0400

    Add txn packing and unpacking

--- blockchain/tests/test_transaction.py
+++ blockchain/tests/test_transaction.py
@@ -7,6 +7,29 @@ from toycoin import transaction # type: ignore
 
 
 ################################################################################
+# Sample Data
+
+
+txn0a = {'previous_hashes': [b'0', b'1'],
+         'receiver': b'receiver_public',
+         'receiver_value': 100,
+         'receiver_signature': b'receiver_signature',
+         'sender': b'genesis',
+         'sender_change': 0,
+         'sender_signature': b'sender_signature'
+         }
+
+
+txn0b = {'previous_hashes': [b'2'],
+         'receiver': b'receiver2_public',
+         'receiver_value': 50,
+         'receiver_signature': b'receiver2_signature',
+         'sender': b'genesis',
+         'sender_change': 0,
+         'sender_signature': b'sender2_signature'
+         }
+
+################################################################################
 
 
 class TestTransactionHelpers:
@@ -18,13 +41,26 @@ class TestTransactionHelpers:
         t1 = {'txn_hash': b'',
               'owner': b'',
               'value': 100,
-              'signature': b''}
+              'signature': b''
+              }
 
         t2 = {'txn_hash': b'',
               'owner': b'',
-              'value': 100,
-              'signature': b''}
+              'value': 50,
+              'signature': b''
+              }
 
         assert f([]) == 0
         assert f([t1]) == 100
-        assert f([t1, t2]) == 200
+        assert f([t1, t2]) == 150
+
+
+    def test_pack_unpack(self):
+        """Test round trip pack and unpack"""
+
+        f = transaction.pack
+        g = transaction.unpack
+
+        assert g(f(txn0a)) == txn0a
+        assert g(f(txn0b)) == txn0b
+        assert g(f(txn0a)) != txn0b
